e6c1fac20921135af6cbb92488670acc8ec77d8c
aleksandrbadanin <sashabadanin147@gmail.com>
2026-02-15 21:04:39 +0300

    Слово начинается с ... [#505233]

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenStartWithPrefixThenTrue2() {
+        char[] word = {'H'};
+        char[] prefix = {'H'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+}
