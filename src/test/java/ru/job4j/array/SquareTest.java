48c2c4fdc5cce338ac3d7a7f3e2cad548af6c5c4
aleksandrbadanin <sashabadanin147@gmail.com>
2026-02-15 16:34:28 +0300

    Массивы в тестах [#505226]

--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -15,7 +15,7 @@ class SquareTest {
     }
 
     @Test
-    void whenBound5Then01468() {
+    void whenBound5Then014916() {
         int bound = 5;
         int[] result = Square.calculate(bound);
         int[] expected = new int[] {0, 1, 4, 9, 16};
